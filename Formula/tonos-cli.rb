3f7b0b8df0342eca80aa879a62ff1b66ba870183
Ilyar <ilyar.software@gmail.com>
2020-11-08 09:15:57 +0200

    update: change version 0.1.25 for tonos-cli

--- Formula/tonos-cli.rb
+++ Formula/tonos-cli.rb
@@ -1,18 +1,22 @@
 class TonosCli < Formula
   desc "Command line tool for TON OS"
   homepage "https://github.com/tonlabs/tonos-cli"
-  url "https://github.com/tonlabs/tonos-cli/archive/v0.1.22.tar.gz"
-  sha256 "f49d22bcf9660fe0cea7659a4de2ea78547203793bbd37a059ae55147986d4a0"
+  url "https://github.com/tonlabs/tonos-cli/archive/v0.1.25.tar.gz"
+  sha256 "ca667e95763da14240ab3b2190378b72ebff2d055e7d9ca4f4250b3c43080941"
+  head "https://github.com/tonlabs/tonos-cli.git"
   license "Apache-2.0"
 
   depends_on "rust" => :build
-
+  depends_on "docker" => :build
   def install
-    # TODO system "cargo", "test"
+    system "docker", "run", "--name", "test-tonos-cli-#{version}", "--network", "host", "--detach", "tonlabs/local-node"
+    system "cargo", "test", "--", "--test-threads", "1"
+    system "docker", "rm", "--force", "test-tonos-cli-#{version}"
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    system "#{bin}/tonos-cli", "--version"
+    output = shell_output("#{bin}/tonos-cli --version")
+    assert_match "#{version}", output
   end
 end
