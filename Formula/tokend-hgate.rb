9f19c20541e7c63e341bce97aa5cc075963b4344
Ilyar <ilyar.software@gmail.com>
2021-01-26 19:17:23 +0200

    fix: fix length description

--- Formula/tokend-hgate.rb
+++ Formula/tokend-hgate.rb
@@ -1,5 +1,5 @@
 class TokendHgate < Formula
-  desc "Hgate uses keypair provided in config file to sign and submit transactions into TokenD"
+  desc "Hgate uses keypair provided in config file to sign and submit into TokenD"
   homepage "https://tokend.gitlab.io/hgate/"
   url "https://gitlab.com/tokend/hgate/-/archive/1.1.0/hgate-1.1.0.tar.gz"
   sha256 "2d6f5352a7e94a84ecbd2589b080ec9e1a3c6ea71b523ce6c4f9e5ab9acc011f"
