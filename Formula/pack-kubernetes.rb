f07bf2cd48cabc503d4b2cd2199fc367a4bd8f21
Ilyar <ilyar.software@gmail.com>
2021-01-26 18:41:31 +0200

    fix: fix brew style

--- Formula/pack-kubernetes.rb
+++
@@ -1,8 +0,0 @@
-class PackKubernetes < Formula
-    url "file:///dev/null"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    version "1.0.0"
-    depends_on "kubectl"
-    depends_on "helm"
-    depends_on "kops"
-end
--- Formula/tokend-cli.rb
+++ Formula/tokend-cli.rb
@@ -1,22 +1,22 @@
 class TokendCli < Formula
-    desc "Production Grade TokenD Installation"
-    homepage "https://gitlab.com/tokend/tokend-cli"
-    url "https://gitlab.com/tokend/tokend-cli/-/archive/v4.2.1/tokend-cli-v4.2.1.tar.gz"
-    sha256 "9f3f1218992ca0e6db6d9f42a1970a94f0a9fd8f0a9624101b4611d84728e868"
-    license "Apache-2.0"
-    head "https://gitlab.com/tokend/tokend-cli.git"
+  desc "Production Grade TokenD Installation"
+  homepage "https://gitlab.com/tokend/tokend-cli"
+  url "https://gitlab.com/tokend/tokend-cli/-/archive/v4.2.1/tokend-cli-v4.2.1.tar.gz"
+  sha256 "9f3f1218992ca0e6db6d9f42a1970a94f0a9fd8f0a9624101b4611d84728e868"
+  license "Apache-2.0"
+  head "https://gitlab.com/tokend/tokend-cli.git"
 
-    depends_on "go" => :build
+  depends_on "go" => :build
 
-    def install
-        ENV["GOPATH"] = buildpath
-        (buildpath/"src/gitlab.com/tokend/tokend-cli").install buildpath.children
-        cd "src/gitlab.com/tokend/tokend-cli" do
-        system "go", "build", "-o", bin/"tokend", "internal/cmd/genconfig.go"
-        end
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/gitlab.com/tokend/tokend-cli").install buildpath.children
+    cd "src/gitlab.com/tokend/tokend-cli" do
+    system "go", "build", "-o", bin/"tokend", "internal/cmd/genconfig.go"
     end
+  end
 
-    test do
-        system "#{bin}/tokend", "help"
-    end
+  test do
+    system "#{bin}/tokend", "help"
+  end
 end
