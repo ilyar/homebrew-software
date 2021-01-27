60ae143c877cf9728bad092799d4b6888f85a188
Ilyar <ilyar.software@gmail.com>
2021-01-27 08:05:29 +0000

    tonos-cli: add 0.2.0 bottle.

--- Formula/tonos-cli.rb
+++ Formula/tonos-cli.rb
@@ -7,6 +7,13 @@ class TonosCli < Formula
   license "Apache-2.0"
   head "https://github.com/tonlabs/tonos-cli.git"
 
+  bottle do
+    root_url "https://github.com/ilyar/homebrew-software/releases/download/tonos-cli-0.2.0"
+    cellar :any_skip_relocation
+    sha256 "fdea3dcb3d854a72fa246cd396f74c7ecbe3d0f398c4d673ecd004f375ccb5ab" => :catalina
+    sha256 "ef8d42ce8d6471eeee7bbc8fab06d0ae1615c627380111fb496f8995d4ef3500" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
   depends_on "openssl"
 
