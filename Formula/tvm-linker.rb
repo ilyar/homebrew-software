036e9e3128a183b5604f14f3ab94ebbb9e11aaa5
Ilyar <ilyar.software@gmail.com>
2021-01-27 13:13:55 +0000

    tvm-linker: add 2001271811 bottle.

--- Formula/tvm-linker.rb
+++ Formula/tvm-linker.rb
@@ -7,6 +7,13 @@ class TvmLinker < Formula
   license "Apache-2.0"
   head "https://github.com/tonlabs/TVM-linker.git"
 
+  bottle do
+    root_url "https://github.com/ilyar/homebrew-software/releases/download/tvm-linker-2001271811"
+    cellar :any_skip_relocation
+    sha256 "fef71476f518c2c9536318e536c2156733c9d5add488362df08f72083407c5ca" => :catalina
+    sha256 "c5dc667f261ae49a7e925fd1b71d5d6154badb1795cdf332bf2a8ed634b7bdff" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
 
   def install
